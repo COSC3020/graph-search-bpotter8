0804a21fd8cfcfc80ffae5b37c2b25976c7312cc
Braden Potter <144744976+bpotter8@users.noreply.github.com>
2024-04-17 00:29:02 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,29 @@
 function depthFirstSearch(graph, startNode, targetNode) {
+    var routeTaken = [];
+    var visited = [];
+    var stack = [[startNode]];
+
+    while (stack.length) {
+        var currentPath = stack.pop();
+        var currentNode = currentPath[currentPath.length - 1];
+
+        if (!visited.includes(currentNode)) {
+            routeTaken.push(currentNode);
+            visited.push(currentNode);
+
+            if (currentNode === targetNode) {
+                return currentPath;
+            }
+            var attachedNodes = graph[currentNode];
+
+            for (var attachedNode of attachedNodes) {
+                if (!visited.includes(attachedNode)) {
+                    var bestPath = currentPath.slice();
+                    bestPath.push(attachedNode);
+                    stack.push(bestPath);
+                }
+            }
+        }
+    }
     return [];
 }
