0a665a858574d13cec91a9587886167de042e795
Braden Potter <144744976+bpotter8@users.noreply.github.com>
2024-04-17 12:19:35 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -12,4 +12,4 @@ const testGraph = {
   F: []
 }
 
-jsc.assert(jsc.utils.isEqual(depthFirstSearch(testGraph, 'A', 'F')), (['A', 'C', 'D', 'F']));
+jsc.assert(JSON.stringify(depthFirstSearch(testGraph, 'A', 'F')) == JSON.stringify(['A', 'C', 'D', 'F']))
