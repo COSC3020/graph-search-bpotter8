2fc0529fe4520b25bba5e4bf5bd0c574a48b3f75
Braden Potter <144744976+bpotter8@users.noreply.github.com>
2024-04-17 12:43:17 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -13,4 +13,23 @@ const testGraph = {
   F: []
 }
 
+//Unit testing for graph movement between two nodes
 assert(JSON.stringify(depthFirstSearch(testGraph, 'A', 'F')) == JSON.stringify(['A', 'C', 'D', 'F']))
+assert(JSON.stringify(depthFirstSearch(testGraph, 'A', 'E')) == JSON.stringify(['A', 'B', 'E']))
+assert(JSON.stringify(depthFirstSearch(testGraph, 'C', 'F')) == JSON.stringify(['C', 'D', 'F']))
+assert(JSON.stringify(depthFirstSearch(testGraph, 'B', 'F')) == JSON.stringify(['B', 'C', 'D', 'F']))
+assert(JSON.stringify(depthFirstSearch(testGraph, 'A', 'C')) == JSON.stringify(['A', 'C']))
+
+//Unit testing for the starting node and target node being the same
+assert(JSON.stringify(depthFirstSearch(testGraph, 'A', 'A')) == JSON.stringify(['A']))
+assert(JSON.stringify(depthFirstSearch(testGraph, 'B', 'B')) == JSON.stringify(['B']))
+assert(JSON.stringify(depthFirstSearch(testGraph, 'C', 'C')) == JSON.stringify(['C']))
+assert(JSON.stringify(depthFirstSearch(testGraph, 'D', 'D')) == JSON.stringify(['D']))
+assert(JSON.stringify(depthFirstSearch(testGraph, 'E', 'E')) == JSON.stringify(['E']))
+
+//Unit testing for no path available between two nodes
+assert(JSON.stringify(depthFirstSearch(testGraph, 'E', 'D')) == JSON.stringify([]))
+assert(JSON.stringify(depthFirstSearch(testGraph, 'F', 'A')) == JSON.stringify([]))
+assert(JSON.stringify(depthFirstSearch(testGraph, 'E', 'A')) == JSON.stringify([]))
+assert(JSON.stringify(depthFirstSearch(testGraph, 'D', 'B')) == JSON.stringify([]))
+assert(JSON.stringify(depthFirstSearch(testGraph, 'B', 'A')) == JSON.stringify([]))
